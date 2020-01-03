a52c7400e3773af041c359bdd1b46addf12fac35
Navidarito Chen Cen <superlocox@hotmail.es>
2020-01-03 04:17:59 -0400

    base de datos

--- src/database.js
+++ src/database.js
@@ -4,15 +4,16 @@ mongoose.set('useFindAndModify', false);
 
 
 
-/*mongoose.connect('mongodb+srv://navidarito:!david123@cluster0-46pmk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+//*
+mongoose.connect('mongodb+srv://navidarito:!david123@cluster0-46pmk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
 })  .then(db => console.log('DB is connected'))
 .catch(err => console.error(err));
-*/
-//*
+//*/
+/*
 
 mongoose.connect('mongodb://localhost/node-notes-db', {
   useCreateIndex: true,
@@ -22,4 +23,4 @@ mongoose.connect('mongodb://localhost/node-notes-db', {
 })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
-//*/
+/*/
