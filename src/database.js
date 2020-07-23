5642f32f2b8a174bbdf084c1c3e76a49d6051aed
superlocox <superlox@hotmail.es>
2020-07-23 00:07:19 -0400

    commit

--- src/database.js
+++ src/database.js
@@ -4,22 +4,22 @@ mongoose.set('useFindAndModify', false);
 
 
 
-//*
-// mongoose.connect('mongodb+srv://navidarito:!david123@cluster0-46pmk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-  
-// })  .then(db => console.log('DB is connected'))
-// .catch(err => console.error(err));
-//*/
 
-
-mongoose.connect('mongodb://localhost/node-notes-db', {
+mongoose.connect('mongodb+srv://navidarito:!david123@cluster0-46pmk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.error(err));
+})  .then(db => console.log('DB is connected'))
+.catch(err => console.error(err));
+
+
+
+// mongoose.connect('mongodb://localhost/node-notes-db', {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+  
+// })
+//   .then(db => console.log('DB is connected'))
+//   .catch(err => console.error(err));
